70bc995ad2ffbd23d574530680183364012993b5
slikts <dabas@untu.ms>
2019-10-20 16:34:15 +0300

    test(tuple): spread params

--- test/Tuple.test.ts
+++ test/Tuple.test.ts
@@ -2,16 +2,21 @@ import Tuple from '../src/Tuple';
 
 describe('Tuple', () => {
   const a = Object('a');
+  const { tuple } = Tuple;
   it('constructor throws', () => {
     expect(() => new (Tuple as any)([1, {}], null)).toThrow();
   });
 
   it('static method constructs', () => {
-    expect(Tuple.tuple(1, {})).toBeInstanceOf(Tuple);
+    expect(tuple(1, {})).toBeInstanceOf(Tuple);
   });
 
   it('iterates', () => {
-    expect([...Tuple.tuple(1, a)[Symbol.iterator]()]).toEqual([1, a]);
-    expect([...Tuple.tuple(1, a)]).toEqual([1, a]);
+    expect([...tuple(1, a)[Symbol.iterator]()]).toEqual([1, a]);
+    expect([...tuple(1, a)]).toEqual([1, a]);
+  });
+
+  it('can take spread params', () => {
+    expect(tuple(...([1, a] as const))).toEqual([1, a]);
   });
 });
