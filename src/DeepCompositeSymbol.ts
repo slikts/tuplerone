e87ed21d69a14e8590a6e670632483cd90d78c39
slikts <dabas@untu.ms>
2020-02-22 12:47:42 +0000

    docs: comment `DeepCompositeSymbol`

--- src/DeepCompositeSymbol.ts
+++ src/DeepCompositeSymbol.ts
@@ -1,6 +1,10 @@
 import Tuple from './Tuple';
 import { isObject } from './helpers';
 
+/**
+ * Recursively creates a "composite key" (like a "value identity") for
+ * an object's entries (key-value pairs).
+ */
 // tslint:disable-next-line: variable-name
 const DeepCompositeSymbol = ((object: any, keyFilter?: (key: string) => boolean) => {
   const entries = keyFilter
