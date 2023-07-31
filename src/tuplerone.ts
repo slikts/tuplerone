4e6aabf0cd4df02e5b14fe270e7a3612b8daf9bb
slikts <dabas@untu.ms>
2023-07-31 19:49:37 +0300

    chore: fix formatting

--- src/tuplerone.ts
+++ src/tuplerone.ts
@@ -1,3 +1,3 @@
-export { Tuple, isRef } from './Tuple'
-export { getDeepSymbol } from './deep'
-export { memoize } from './memoize'
+export { Tuple, isRef } from './Tuple';
+export { getDeepSymbol } from './deep';
+export { memoize } from './memoize';
