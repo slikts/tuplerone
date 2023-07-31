b1390cebe4fd08600f75034fc88d42863863a84f
slikts <dabas@untu.ms>
2023-07-31 18:03:13 +0300

    feat: add deep symbols

---
+++ src/deep.spec.ts
@@ -0,0 +1,36 @@
+import { getDeepSymbol } from './deep'
+
+describe('DeepCompositeSymbol', () => {
+  it('returns symbol', () => {
+    expect(typeof getDeepSymbol({})).toBe('symbol')
+  })
+
+  it('supports empty objects and arrays', () => {
+    expect(getDeepSymbol({})).toStrictEqual(getDeepSymbol({}))
+    expect(getDeepSymbol([])).toStrictEqual(getDeepSymbol([]))
+  })
+
+  it('structurally equals shallow object', () => {
+    expect(getDeepSymbol({ a: 1, b: 2 })).toStrictEqual(getDeepSymbol({ a: 1, b: 2 }));
+    expect(getDeepSymbol({ a: 1, b: 2, c: 3 })).not.toStrictEqual(getDeepSymbol({ a: 1, b: 2 }));
+  });
+
+  it('supports arrays', () => {
+    expect(getDeepSymbol([1,2,3])).toStrictEqual(getDeepSymbol([1,2,3]))
+    expect(getDeepSymbol([1,2,3,4])).not.toStrictEqual(getDeepSymbol([1,2,3]))
+  })
+
+  it('supports deep nesting', () => {
+    expect(getDeepSymbol([1, [2, 3]])).toStrictEqual(getDeepSymbol([1, getDeepSymbol([2, 3])]))
+  })
+
+  it.skip("doesn't equal structurally different object", () => {
+    expect(getDeepSymbol({ a: 1, b: 2 })).not.toStrictEqual(getDeepSymbol({ a: 1, b: 3 }));
+  });
+
+  it.skip('supports deep structural equality', () => {
+    const o = () => ({ a: { c: 1 }, b: 2 });
+    expect(getDeepSymbol(o())).toStrictEqual(getDeepSymbol(o()));
+    expect(getDeepSymbol(o())).not.toStrictEqual(getDeepSymbol({}));
+  });
+});
---
+++ src/deep.ts
@@ -0,0 +1,28 @@
+import { getSymbol } from "./graph";
+
+export const getDeepSymbol = (target: unknown): symbol => {
+  const result = _get(target)
+  if (typeof result !== 'symbol') {
+    throw TypeError('Primitive values are not valid targets')
+  }
+  return result
+}
+
+const _get = <A>(edge: A): A | symbol => {
+  switch(typeof edge) {
+    case "string":
+    case "symbol":
+    case "number":
+    case "boolean":
+    case "bigint":
+    case "undefined":
+    case "function":
+      return edge
+    default:
+      if (Array.isArray(edge)) {
+        return getSymbol(edge.map(_get))
+      }
+      return getSymbol(Object.entries(edge as {})
+        .flatMap((value, i): unknown => i % 2 ? _get(value) : value))
+  }
+}
