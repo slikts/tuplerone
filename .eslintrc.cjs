22cc14dc21e03d5f4075fdf0283356f37010cb9e
slikts <dabas@untu.ms>
2023-08-02 00:09:16 +0300

    chore: update lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
       rules: {
         'tsdoc/syntax': 'warn',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
       },
     },
     {
