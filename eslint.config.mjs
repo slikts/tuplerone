2e0236160844c34cdd26b0f762320500291c577a
Reinis Ivanovs <dabas@untu.ms>
2026-02-28 22:37:18 +0200

    chore: update dependencies and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,7 @@ export default [
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-unsafe-function-type': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
     },
   },
 ];
