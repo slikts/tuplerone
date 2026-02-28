bc7b680b7e7f19da73e9e2264aaca8e577da7965
Reinis Ivanovs <dabas@untu.ms>
2026-02-28 21:48:26 +0200

    refactor(WeakishMap): use ES private fields and WeakMap symbol support

--- src/WeakishMap.ts
+++ src/WeakishMap.ts
@@ -1,5 +1,5 @@
-import { GenericMap } from './types';
-import { isObject } from './helpers';
+import type { GenericMap } from './types';
+import { isWeakMapKey } from './helpers';
 
 /**
  * A generic wrapper to Map and WeakMap that can use both non-primitives
@@ -7,39 +7,39 @@ import { isObject } from './helpers';
  * storage as needed.
  */
 export default class WeakishMap<A, B> implements GenericMap<A, B> {
-  private weakMap?: WeakMap<object, B>;
-  private map?: Map<A, B>;
+  #weakMap?: WeakMap<any, B>;
+  #map?: Map<A, B>;
 
   set(k: A, v: B): this {
-    if (isObject(k)) {
-      if (!this.weakMap) {
-        this.weakMap = new WeakMap();
+    if (isWeakMapKey(k)) {
+      if (!this.#weakMap) {
+        this.#weakMap = new WeakMap();
       }
-      this.weakMap.set(k, v);
+      this.#weakMap.set(k, v);
     } else {
-      if (!this.map) {
-        this.map = new Map();
+      if (!this.#map) {
+        this.#map = new Map();
       }
-      this.map.set(k, v);
+      this.#map.set(k, v);
     }
     return this;
   }
 
   get(k: A): B | undefined {
-    if (isObject(k) && this.weakMap) {
-      return this.weakMap.get(k);
+    if (isWeakMapKey(k) && this.#weakMap) {
+      return this.#weakMap.get(k);
     }
-    if (this.map) {
-      return this.map.get(k);
+    if (this.#map) {
+      return this.#map.get(k);
     }
   }
 
   has(k: A): boolean {
-    if (isObject(k) && this.weakMap) {
-      return this.weakMap.has(k);
+    if (isWeakMapKey(k) && this.#weakMap) {
+      return this.#weakMap.has(k);
     }
-    if (this.map) {
-      return this.map.has(k);
+    if (this.#map) {
+      return this.#map.has(k);
     }
     return false;
   }
