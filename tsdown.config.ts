24c59bb02a462a0ba33a35f4e1509192808bf119
Reinis Ivanovs <dabas@untu.ms>
2026-03-01 17:38:05 +0200

    fix: correctly initialize Husky and commitlint

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: ['src/tuplerone.ts'],
-  format: ['es', 'cjs', 'umd'],
+  format: ['esm'],
   globalName: 'tuplerone',
   dts: true,
   clean: true,
   sourcemap: true,
   outDir: 'dist',
+  exports: {
+    devExports: true,
+  },
 });
