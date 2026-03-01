51121dd00f3b38a921a35fdf69e2458510764133
Reinis Ivanovs <dabas@untu.ms>
2026-03-01 18:51:45 +0200

    chore(docs): add metadataBase to layout

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,10 +1,19 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://slikts.github.io/tuplerone',
+  ),
+};
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
