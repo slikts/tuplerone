f47827b3f4c4e6ec809d79276e20464efa6c4e8f
Reinis Ivanovs <dabas@untu.ms>
2026-03-02 21:58:47 +0200

    fix: refine memoize types

--- src/memoize.ts
+++ src/memoize.ts
@@ -1,14 +1,14 @@
 import { getLeaf } from './Tuple';
 import { getDefaultLazy } from './helpers';
 
-type Fn = (this: unknown, ...args: readonly unknown[]) => unknown;
+type Fn = (this: unknown, ...args: never[]) => unknown;
 
 const defaultCache = new WeakMap<object, unknown>();
 
 export const memoize = <A extends Fn>(fn: A, cache = defaultCache): A => {
-  const memoized: Fn = function (this: unknown, ...args: readonly unknown[]): unknown {
+  const memoized: Fn = function (this: unknown, ...args: never[]): unknown {
     const node = getLeaf([memoized, this, ...args]);
-    return getDefaultLazy(node, () => fn.apply(this, args as unknown[]) as unknown, cache);
+    return getDefaultLazy(node, () => fn.apply(this, args as never[]) as unknown, cache);
   };
-  return memoized as unknown as A;
+  return memoized as A;
 };
